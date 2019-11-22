cfdcc1878daeb732d544f51010188e88b9a9ec94
White.Kevin.s651670 <s651670@rrisd.local>
2019-11-22 12:05:31 -0600

    adding timer

--- src/Laser.java
+++ src/Laser.java
@@ -1,4 +1,20 @@
 public class Laser
 {
-  
+
+    // seconds untill laer is destroyed
+  int SecPassed = 0;
+          Timer Timer = new Timer();
+  TimerTask task new = new TimerTask ()
+{
+    public void run()
+    {
+        SecPassed++;
+
+    }
+
+}
+public void start()
+{
+myTimer.scheduleAtFixedRate(task,1000,1000);
+}
 }
