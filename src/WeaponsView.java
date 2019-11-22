67bf65c70b8d1eb4c322b4811f08dc89a464adba
White.Kevin.s651670 <s651670@rrisd.local>
2019-11-22 11:31:57 -0600

    to do list

--- src/WeaponsView.java
+++ src/WeaponsView.java
@@ -3,5 +3,12 @@ import java.util.*;
 
 public class WeaponsView 
 {
+  //To DO:
+  //Make A Small White Line That Will Delete itself after 10 seconds
+  //Move turret
+  //detect direction of turret
+  //Reload meter (talk with Eng. group to find out how long to reload)
+  //Aiming Retical (Different Color? is it different because of energy?)
+  //Talk With Ast. group about how astroids will ushinteract with our bullets and how energy can change that
   
 }
