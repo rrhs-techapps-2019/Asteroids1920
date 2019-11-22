a98aaa42ab128ed8474221c7315babfe25f8414d
Cole Bryant <s632474@student.roundrockisd.org>
2019-11-22 11:24:49 -0600

    Reformat source files

--- src/RunnerServer.java
+++ src/RunnerServer.java
@@ -5,40 +5,40 @@ import mayflower.*;
 public class RunnerServer extends MayflowerHeadless
 {
     //Construct a Mayflower Window.
-    public RunnerServer() 
+    public RunnerServer()
     {
-     //Open a GUI Window with the specified title, width, and height
-     //DO NOT WRITE ANY ADDITIONAL CODE IN THIS CONSTRUCTOR!
-     //You should only change the arguments to the super method.
-     super("Asteroids", 800, 600);
+        //Open a GUI Window with the specified title, width, and height
+        //DO NOT WRITE ANY ADDITIONAL CODE IN THIS CONSTRUCTOR!
+        //You should only change the arguments to the super method.
+        super("Asteroids", 800, 600);
     }
-    
+
     //The init method is called as soon as the Mayflower window is opened.
     //This is where you will setup the initial state of your game
     //ie, create the first world of your game
     public void init()
-    {   
-      //Show a black border around the Actors
-      //Mayflower.showBounds(true);
-      
-      
-     //Create a new instance of a BlockWorld
-     World startingWorld = new ServerWorld();
-     
-     Server server = new Server(startingWorld);
-     
-     //Load the startingWorld into the Mayflower Window
-     Mayflower.setWorld(startingWorld);
+    {
+        //Show a black border around the Actors
+        //Mayflower.showBounds(true);
+
+
+        //Create a new instance of a BlockWorld
+        World startingWorld = new ServerWorld();
+
+        Server server = new Server(startingWorld);
+
+        //Load the startingWorld into the Mayflower Window
+        Mayflower.setWorld(startingWorld);
     }
-    
+
     //This is the first method that is called when you run your program
     //It is the starting point of your program.
     public static void main(String[] args)
     {
-     //Instantiate an instance of this Runner class.
-     // The constructor will be called
-     // Then the init() method will be called
-     //DO NOT WRITE ANY ADDITIONAL CODE IN THIS METHOD!
-     new RunnerServer();
+        //Instantiate an instance of this Runner class.
+        // The constructor will be called
+        // Then the init() method will be called
+        //DO NOT WRITE ANY ADDITIONAL CODE IN THIS METHOD!
+        new RunnerServer();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,124 +1,122 @@
 import mayflower.*;
+
 import java.util.*;
 
 public class Server extends mayflower.net.Server
 {
-  private World world;
-  private Ship ship;
-  private Map<Integer, NetworkActor> actors;
-  private int nextActorId;
-  
-  public Server(World world)
-  {
-    super(8080);
-    actors = new HashMap<>();
-    nextActorId = 1;
-    this.world = world;
-    ship = new Ship(0);
-    actors.put(ship.getId(), ship);
-    world.addObject(ship, world.getWidth()/2, world.getHeight()/2);
-    
-    addAsteroid(10, 10, 10, 1);
-    addAsteroid(600, 200, -100, 1);
-    System.out.println("Server started...");
-  }
-  
-  public void addAsteroid(int x, int y, int direction, int speed)
-  {
-    Asteroid asteroid = new Asteroid(nextActorId++);
-    world.addObject(asteroid, x, y);
-    asteroid.setRotation(direction);
-    asteroid.setSpeed(speed);
-    actors.put(asteroid.getId(), asteroid);
-    System.out.println(asteroid);
-    send("add asteroid " + asteroid);
-  }
-  
-   /**
-    * Handle a message from a client
-    * 
-    * @param id the id of the client that send the message
-    * @param message the message send from the client
-    */
-  public void process(int id, String message)
-  {
-    System.out.println("Process ("+id+"): " + message);
-    
-    String[] parts = message.split(" ");
-    String action = parts[0];
-    
-    String type, direction;
-    switch(action)
+    private World world;
+    private Ship ship;
+    private Map<Integer, NetworkActor> actors;
+    private int nextActorId;
+
+    public Server(World world)
     {
-      case "update":
-        //update the state of all the actors in the world
-        for(NetworkActor actor : actors.values())
-        {
-          send(id, "add " + actor.getType() + " " + actor);
-        }
-        break;
-      case "move":
-        //make ship start moving
-        ship.setSpeed(1);
-        send("update ship "+ship);
-        break;
-      case "turn":
-        //make ship start turning
-        direction = parts[1];
-        if("left".equals(direction))
-        {
-          ship.setRotationSpeed(-1);
-        }
-        else
-        {
-          ship.setRotationSpeed(1);
-        }
-        
-        
-       
-        send("update ship " + ship);
-        break;
-      case "stop":
-        type = parts[1];
-        
-        if("turn".equals(type))
-        {
-          //make ship stop turning
-          ship.setRotationSpeed(0);
-        }
-        else
-        {
-          //make ship stop moving
-          ship.setSpeed(0);
-        }
-        
-        send("update ship " + ship);
-        break;
+        super(8080);
+        actors = new HashMap<>();
+        nextActorId = 1;
+        this.world = world;
+        ship = new Ship(0);
+        actors.put(ship.getId(), ship);
+        world.addObject(ship, world.getWidth() / 2, world.getHeight() / 2);
+
+        addAsteroid(10, 10, 10, 1);
+        addAsteroid(600, 200, -100, 1);
+        System.out.println("Server started...");
     }
-  }
-  
-  /**
-   * Handle a new connection to the server
-   * 
-   * @param id the id of the client that just connected to the server
-   */
-  public void onJoin(int id)
-  {
-    System.out.println("Joined: " + id);
-    
-    for(NetworkActor actor : actors.values())
+
+    public void addAsteroid(int x, int y, int direction, int speed)
     {
-      send(id, "add " + actor.getType() + " " + actor);
+        Asteroid asteroid = new Asteroid(nextActorId++);
+        world.addObject(asteroid, x, y);
+        asteroid.setRotation(direction);
+        asteroid.setSpeed(speed);
+        actors.put(asteroid.getId(), asteroid);
+        System.out.println(asteroid);
+        send("add asteroid " + asteroid);
     }
-  }
-  
-  /**
-   * Handle a client that disconnected from the server
-   * 
-   * @param id the id of the client that disconnected
-   */
-  public void onExit(int id)
-  {
-    System.out.println("Disconnected: " + id);
-  }
+
+    /**
+     * Handle a message from a client
+     *
+     * @param id      the id of the client that send the message
+     * @param message the message send from the client
+     */
+    public void process(int id, String message)
+    {
+        System.out.println("Process (" + id + "): " + message);
+
+        String[] parts = message.split(" ");
+        String action = parts[0];
+
+        String type, direction;
+        switch (action)
+        {
+            case "update":
+                //update the state of all the actors in the world
+                for (NetworkActor actor : actors.values())
+                {
+                    send(id, "add " + actor.getType() + " " + actor);
+                }
+                break;
+            case "move":
+                //make ship start moving
+                ship.setSpeed(1);
+                send("update ship " + ship);
+                break;
+            case "turn":
+                //make ship start turning
+                direction = parts[1];
+                if ("left".equals(direction))
+                {
+                    ship.setRotationSpeed(-1);
+                } else
+                {
+                    ship.setRotationSpeed(1);
+                }
+
+
+                send("update ship " + ship);
+                break;
+            case "stop":
+                type = parts[1];
+
+                if ("turn".equals(type))
+                {
+                    //make ship stop turning
+                    ship.setRotationSpeed(0);
+                } else
+                {
+                    //make ship stop moving
+                    ship.setSpeed(0);
+                }
+
+                send("update ship " + ship);
+                break;
+        }
+    }
+
+    /**
+     * Handle a new connection to the server
+     *
+     * @param id the id of the client that just connected to the server
+     */
+    public void onJoin(int id)
+    {
+        System.out.println("Joined: " + id);
+
+        for (NetworkActor actor : actors.values())
+        {
+            send(id, "add " + actor.getType() + " " + actor);
+        }
+    }
+
+    /**
+     * Handle a client that disconnected from the server
+     *
+     * @param id the id of the client that disconnected
+     */
+    public void onExit(int id)
+    {
+        System.out.println("Disconnected: " + id);
+    }
 }
