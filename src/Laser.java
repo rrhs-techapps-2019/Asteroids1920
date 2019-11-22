1aab5c30b4c4d49098ac4c289ace6ac2e864e62c
Cantu.Gianni.s128821 <s128821@rrisd.local>
2019-11-22 11:51:56 -0600

    Add constructor for laser

--- src/Laser.java
+++ src/Laser.java
@@ -1,4 +1,13 @@
-public class Laser
+import mayflower.*;
+
+public class Laser extends NetworkActor
 {
-  
+  // TO DO:
+  // Move laser in space 
+  // Add collsion event with asteroid
+    public Laser(int id) {
+        super(id, "laser");
+        MayflowerImage img = new MayflowerImage("Laser.png");
+        setImage(img);
+    }
 }
