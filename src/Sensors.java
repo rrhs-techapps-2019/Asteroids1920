e4c075c9af9b1facde37ff16da3c5b6c4e29cd05
EAusman <emma.ausman@gmail.com>
2019-11-22 11:28:47 -0600

    created sensors class

---
+++ src/Sensors.java
@@ -0,0 +1,10 @@
+import mayflower.*;
+import java.util.*;
+
+public class Sensors extends GameView
+{
+  //800 by 600 screen per person
+  public Sensors(Client client, GameState state){
+    super(client,state);
+  }
+}
