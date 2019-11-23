64ca3adbf3de066f7b6cabd365aa257358135e86
cbryant02 <3ef4c1bb@opayq.com>
2019-11-22 23:14:37 -0600

    Resolve errors in WeaponsView

---
+++ src/org/rrhs/asteroids/views/WeaponsView.java
@@ -0,0 +1,44 @@
+import mayflower.*;
+import java.util.*;
+
+public class WeaponsView extends GameView
+{
+    //To DO:
+    // [ ] Make A Small White Line That Will Delete itself after 10 seconds
+    // [ ] Move turret
+    // [ ] detect direction of turret
+    // [ ] Reload meter (talk with Eng. group to find out how long to reload)
+    // [ ] Aiming Retical (Different Color? is it different because of energy?)
+    // [ ] Talk With Ast. group about how astroids will ushinteract with our bullets and how energy can change that
+    public WeaponsView(Client client, GameState state)
+    {
+        super(client, state);
+    }
+
+    // Makes turret fire laser
+    public void fireLaser()
+    {
+    }
+
+    // Gets status of reload meter
+    public int getReload()
+    {
+        return -1;
+    }
+
+    // Clears the reload meter
+    public void clearReload()
+    {
+    }
+
+    // Charges the reload meter based on weapon system's energy (parameter might change)
+    public void reload(int energy)
+    {
+    }
+
+    // Gets the rotation of the turrent (To figure out where turret is relative to the ship?)
+    public int getRotation()
+    {
+        return -1;
+    }
+}
