7a2bfcde91b42f91288a56c2b9f8c3e91d5f7527
EAusman <emma.ausman@gmail.com>
2019-12-03 11:47:51 -0600

    fixed merge coflict

--- src/Sensors.java
+++ src/Sensors.java
@@ -28,4 +28,22 @@ public class Sensors extends GameView
         super.act(); 
     }
     //public int Screen 
+   
+  //800 by 600 screen per person
+  
+  //gets ship coordinates, later will compare to other things
+  public void compareCords(){
+    NetworkActor[] actors =getState().getActors();
+    NetworkActor ship = null;
+    for (NetworkActor a:actors){
+      if(a.getType().equals("ship")){
+        ship = a;
+        break;
+      }
+    }
+    if(ship!=null){
+      int shipx = ship.getX();
+      int shipy = ship.getY();
+    }
+  }
 }
