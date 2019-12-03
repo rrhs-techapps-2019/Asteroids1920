a914aeec79742ee57397161c832bbe1e02d294a9
EAusman <emma.ausman@gmail.com>
2019-12-03 12:03:24 -0600

    Added the ability to map the world

--- src/Sensors.java
+++ src/Sensors.java
@@ -4,12 +4,15 @@ import java.util.*;
 public class Sensors extends GameView
 { 
     private ArrayList<NetworkActor> asteroids;
+    private NetworkActor ship;
+    private ArrayList<NetworkActor> visibleAsteroids;
     //800 by 600 screen per person
     public Sensors(Client client, GameState state){
         super(client,state);
         asteroids = getAsteroids(); 
+        ship=compareCords();
     }
-
+    //gets list of asteroids to use later
     public ArrayList<NetworkActor> getAsteroids(){ 
         ArrayList<NetworkActor> ret = new ArrayList<NetworkActor>();
         NetworkActor[] actors = super.getState().getActors();
@@ -24,26 +27,33 @@ public class Sensors extends GameView
 
     public void act()
     {
-        asteroids = getAsteroids();
         super.act(); 
+        ship = compareCords();
+        asteroids = getAsteroids();
     }
     //public int Screen 
-   
-  //800 by 600 screen per person
-  
-  //gets ship coordinates, later will compare to other things
-  public void compareCords(){
-    NetworkActor[] actors =getState().getActors();
-    NetworkActor ship = null;
-    for (NetworkActor a:actors){
-      if(a.getType().equals("ship")){
-        ship = a;
-        break;
-      }
+    public void createScreen(){
+     for(NetworkActor asteroid : visibleAsteroids){
+         addObject(asteroid,asteroid.getX(),asteroid.getY());
+        }
+     addObject(ship,ship.getX(),ship.getY());
     }
-    if(ship!=null){
-      int shipx = ship.getX();
-      int shipy = ship.getY();
+    //800 by 600 screen per person
+
+    //gets ship coordinates, later will compare to other things
+    public NetworkActor compareCords(){
+        NetworkActor[] actors =getState().getActors();
+        NetworkActor ship = null;
+        for (NetworkActor a:actors){
+            if(a.getType().equals("ship")){
+                ship = a;
+                break;
+            }
+        }
+        if(ship!=null){
+            int shipx = ship.getX();
+            int shipy = ship.getY();
+        }
+        return ship;
     }
-  }
 }
