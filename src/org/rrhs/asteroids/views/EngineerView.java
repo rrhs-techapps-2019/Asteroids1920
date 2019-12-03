604b777fb94fdc5c0618236cee0ed15bab910216
ShantanuT01 <48578270+ShantanuT01@users.noreply.github.com>
2019-12-03 11:30:50 -0600

    Testing GitHub Desktop Application
    Testing GitHub Desktop Application
    
    (cherry picked from commit a0f0eaccacbb47e0e11b43c1f4d0d0c76399e080)

--- src/org/rrhs/asteroids/views/EngineerView.java
+++ src/org/rrhs/asteroids/views/EngineerView.java
@@ -5,6 +5,7 @@ import org.rrhs.asteroids.network.Client;
 
 public class EngineerView extends GameView
 {
+    //private int energyPilot = 50;
     public EngineerView(Client client, GameState state) {
 
         super(client, state);
