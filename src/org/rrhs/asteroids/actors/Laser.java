8cdafc4098accec5d68d54f107630b325b74e143
White.Kevin.s651670 <s651670@rrisd.local>
2019-12-03 11:47:37 -0600

    New laser

---
+++ src/org/rrhs/asteroids/actors/Laser.java
@@ -0,0 +1,40 @@
+package org.rrhs.asteroids.actors;
+
+import mayflower.MayflowerImage;
+
+public class Laser
+{
+        public Laser( Ship from, String direction)
+        {
+        this.direction = direction;
+        this.from = from;
+        Picture pic = new Picture("");
+        pic.resize(NA, NA);
+        pic.setBounds(NA, NA, NA, NA);
+        setPicture(pic);
+
+        }
+    public void update()
+    {
+        super.update();
+
+        Actor[] touching = getTouching();
+
+        for(Actor lazer : touching)
+        {
+            if(lazer  instanceof AsteroidBig)
+            {
+                Stage s = getStage();
+                s.removeActor(a);
+                s.removeActor(this);
+            }
+            if(lazer  instanceof AsteroidSmall)
+            {
+                Stage s = getStage();
+                s.removeActor(a);
+                s.removeActor(this);
+            }
+        }
+
+
+    }
