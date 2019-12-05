03fe48cdde2ce5288db157f0d04a93843fe02a93
White.Kevin.s651670 <s651670@rrisd.local>
2019-12-05 11:47:04 -0600

    Laser now Properly communicates with ServerWorld to remove its self when touching an Astroid

--- src/org/rrhs/asteroids/actors/Laser.java
+++ src/org/rrhs/asteroids/actors/Laser.java
@@ -11,40 +11,24 @@ public class Laser extends NetworkActor
         setImage(img);
     }
 }
-// Just read some of the methods here and I just realized that objects can only move by sending it to the server first
-// so everything commnented here wouldn't really work or would make things harder.
-// Anyway I've figured out how it works, but just in case I'm leaving it here.
-// Gianni
 
-//    public Laser( Ship from, String direction)
-//    {
-//        this.direction = direction;
-//        this.from = from;
-//        Picture pic = new Picture("");
-//        pic.resize(NA, NA);
-//        pic.setBounds(NA, NA, NA, NA);
-//        setPicture(pic);
-//    }
-//
-//    public void update() {
-//        super.update();
-//
-//        Actor[] touching = getTouching();
-//
-//        for (Actor lazer : touching) {
-//            if (lazer instanceof AsteroidBig) {
-//                Stage s = getStage();
-//                s.removeActor(AstroidBig);
-//                s.add(AstroidSmall);
-//                s.add(AstroidSmall);
-//                s.removeActor(this);
-//            }
-//            if (lazer instanceof AsteroidSmall) {
-//                Stage s = getStage();
-//                s.removeActor(AstroidSmall);
-//                s.removeActor(this);
-//            }
-//        }
-//    }
+    public void update() {
+      super.update();
 
-}
+        Actor[] touching = getTouching();
+
+        for (Actor laser : touching) {
+            if (laser instanceof AsteroidBig) {
+                ServerWorld W = getServerWorld();
+
+                s.removeActor(this);
+            }
+            if (lazer instanceof AsteroidSmall) {
+                ServerWorld W = getServerWorld();
+
+                w.removeActor(this);
+            }
+        }
+    }
+
+
