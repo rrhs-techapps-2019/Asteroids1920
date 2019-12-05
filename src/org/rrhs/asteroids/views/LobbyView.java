577fb64d592875f030bf5d2a97fed9c92203446d
MilesCrain <milescrain@gmail.com>
2019-12-05 11:38:53 -0600

    Move Lobby to LobbyView

---
+++ src/org/rrhs/asteroids/views/LobbyView.java
@@ -0,0 +1,16 @@
+package org.rrhs.asteroids.views;
+
+import mayflower.World;
+import org.rrhs.asteroids.GameState;
+import org.rrhs.asteroids.network.Client;
+
+public class LobbyView extends GameView {
+    public LobbyView(Client client, GameState state) {
+        super(client, state);
+    }
+
+    @Override
+    public void act() {
+
+    }
+}
