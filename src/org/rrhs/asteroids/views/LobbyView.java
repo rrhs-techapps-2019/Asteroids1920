8e7775cb5515c2e01cb2d1a2a46b3f74fbd89481
Villus007 <pvishwas2011@gmail.com>
2019-12-09 11:24:56 -0600

    added temp variable in if statements

--- src/org/rrhs/asteroids/views/LobbyView.java
+++ src/org/rrhs/asteroids/views/LobbyView.java
@@ -20,22 +20,26 @@ public class LobbyView extends GameView {
         if(Mayflower.isKeyPressed(Keyboard.KEY_1))
         {
             removeText(50,100);
+            Temp("WEAPONS");
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_2))
         {
             removeText(50, 150);
+            Temp("PILOT");
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_3))
         {
             removeText(50, 200);
+            Temp("SENSORS");
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_4))
         {
             removeText(50, 250);
+            Temp("ENGINEER");
         }
     }
-    public void Temp(){
-        super.showText("PLEASE HOLD", 50, 50);
+    public void Temp(String mc){
+        super.showText("PLEASE HOLD" + mc, 50, 50);
         super.showText("OTHERS ARE CHOOSING", 50, 100);
     }
 }
