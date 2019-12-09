d0bdb58fdcf015ede62e41fc7ed82d1ead1540c9
jackson1442 <me@j1442.co>
2019-12-09 11:41:15 -0600

    add initial server actions

--- src/org/rrhs/asteroids/network/actions/NetworkAction.java
+++ src/org/rrhs/asteroids/network/actions/NetworkAction.java
@@ -1,6 +1,8 @@
 package org.rrhs.asteroids.network.actions;
 import org.rrhs.asteroids.network.Server;
 
+import java.util.Map;
+
 public interface NetworkAction {
-    public void act(Server server);
+    public void act(Server server, int clientID, Map<String, String> parsedMessage);
 }
