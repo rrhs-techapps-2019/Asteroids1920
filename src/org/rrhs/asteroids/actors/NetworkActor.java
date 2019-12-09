cd3cdca19a5566743ca93274fa6eff3d96346229
jackson1442 <me@j1442.co>
2019-12-09 12:10:27 -0600

    done for the day ;)

--- src/org/rrhs/asteroids/actors/NetworkActor.java
+++ src/org/rrhs/asteroids/actors/NetworkActor.java
@@ -2,62 +2,60 @@ package org.rrhs.asteroids.actors;
 
 import mayflower.Actor;
 
+import java.awt.geom.Point2D;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
-public class NetworkActor extends Actor
-{
+public class NetworkActor extends Actor {
     private int id;
     private int speed;
     private int rotationSpeed;
     private int direction;
     private String type;
+    private Map<Integer, Point2D> locationHistory;
+    private int tick;
 
-    public NetworkActor(int id, String type)
-    {
+    public NetworkActor(int id, String type) {
         this.id = id;
         this.type = type;
+        locationHistory = new HashMap<>();
+        tick = 0;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public String getType()
-    {
+    public String getType() {
         return type;
     }
 
-    public int getSpeed()
-    {
+    public int getSpeed() {
         return speed;
     }
 
-    public void setSpeed(int speed)
-    {
+    public void setSpeed(int speed) {
         this.speed = speed;
     }
 
-    public int getRotationSpeed()
-    {
+    public int getRotationSpeed() {
         return rotationSpeed;
     }
 
-    public void setRotationSpeed(int speed)
-    {
+    public void setRotationSpeed(int speed) {
         this.rotationSpeed = speed;
     }
 
-    public void move()
-    {
+    public void move() {
         move(speed);
     }
 
-    public void act()
-    {
+    public void act() {
         turn(getRotationSpeed());
         move();
+        ++tick;
     }
 
     public String toString() {
@@ -71,4 +69,44 @@ public class NetworkActor extends Actor
         return data.toString();
     }
 
+    public void setLocation(double x, double y) {
+        super.setLocation(x, y);
+        locationHistory.put(tick, new Point(x, y));
+        while (locationHistory.size() > 50) {
+            locationHistory.remove(getSmallest());
+        }
+    }
+
+    private int getSmallest() {
+        Set<Integer> s = locationHistory.keySet();
+        Integer[] arr = (Integer[])s.toArray();
+        Arrays.sort(arr);
+        return arr[0];
+    }
+
+    class Point extends Point2D {
+        double x;
+        double y;
+
+        Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public double getX() {
+            return x;
+        }
+
+        @Override
+        public double getY() {
+            return y;
+        }
+
+        @Override
+        public void setLocation(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
