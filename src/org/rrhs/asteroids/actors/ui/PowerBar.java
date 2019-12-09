ba86bdbf0108c4df7658c3aff08dc33884adbbc2
Cole Bryant <cole.z.bryant02@gmail.com>
2019-12-09 10:50:52 -0600

    Add PowerBar#setPercentage

--- src/org/rrhs/asteroids/actors/ui/PowerBar.java
+++ src/org/rrhs/asteroids/actors/ui/PowerBar.java
@@ -1,6 +1,7 @@
 package org.rrhs.asteroids.actors.ui;
 
 import mayflower.Actor;
+import mayflower.MayflowerImage;
 import org.rrhs.asteroids.util.MayflowerUtils;
 
 /**
@@ -10,19 +11,28 @@ import org.rrhs.asteroids.util.MayflowerUtils;
  */
 public class PowerBar extends Actor
 {
+    private static final int DEFAULT_WIDTH = 24;
+    private static final int DEFAULT_HEIGHT = 256;
+
     private static int index = 0; // Unique index
 
     public PowerBar()
     {
-        this.setImage(MayflowerUtils.imageFromColor(24,
-                256,
+        this.setImage(MayflowerUtils.imageFromColor(DEFAULT_WIDTH,
+                DEFAULT_HEIGHT,
                 MayflowerUtils.colorFromHsb((45 * index) % 360, 0.66f, 0.8f)));
         index++;
     }
 
     public void setPercentage(final int percentage)
     {
+        int tHeight = (int) ((DEFAULT_HEIGHT / 100.0) * percentage);
+        int deltaY = getHeight() - tHeight;
 
+        MayflowerImage scaled = new MayflowerImage(getImage());
+        scaled.scale(DEFAULT_WIDTH, tHeight);
+        setImage(scaled);
+        setLocation(getX(), getY() + deltaY);
     }
 
     @Override
