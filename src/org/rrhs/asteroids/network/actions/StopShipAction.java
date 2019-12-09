305bb9e373b59864b976103cf2b10555fda35af4
jackson1442 <me@j1442.co>
2019-12-09 11:45:07 -0600

    add remaining initial network actions

---
+++ src/org/rrhs/asteroids/network/actions/StopShipAction.java
@@ -0,0 +1,30 @@
+package org.rrhs.asteroids.network.actions;
+
+import org.rrhs.asteroids.actors.NetworkActor;
+import org.rrhs.asteroids.network.Server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StopShipAction implements NetworkAction {
+    @Override
+    public void act(Server server, int clientID, Map<String, String> parsedMessage) {
+        String action = parsedMessage.get("action");
+        NetworkActor ship = server.getShip();
+        Map<String, String> messageToSend = new HashMap<>();
+
+        if ("turn".equals(action))
+        {
+            //make ship stop turning
+            ship.setRotationSpeed(0);
+        } else
+        {
+            //make ship stop moving
+            ship.setSpeed(0);
+        }
+        messageToSend.put("action", "update");
+        messageToSend.put("type", "ship");
+        messageToSend.put("actor", ship.toString());
+        server.send(messageToSend.toString());
+    }
+}
