2ca5b2f9dd19fe11b80744bbc6fc10491db40e30
EAusman <emma.ausman@gmail.com>
2019-12-09 11:52:28 -0600

    new Class

---
+++ src/Circle.java
@@ -0,0 +1,24 @@
+import mayflower.*;
+/**
+ * Write a description of class Circle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Circle extends Actor
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Circle
+     */
+    public Circle()
+    {
+        MayflowerImage img = new MayflowerImage("img/circle.png");
+        img.scale(40,40);
+        setImage(img);
+    }
+    public void act(){
+        
+    }
+}
