eb0629041740e45e5b646218a7a7987d989d3786
Villus007 <pvishwas2011@gmail.com>
2019-12-09 11:04:34 -0600

    added if statements

--- src/org/rrhs/asteroids/views/LobbyView.java
+++ src/org/rrhs/asteroids/views/LobbyView.java
@@ -1,6 +1,7 @@
 package org.rrhs.asteroids.views;
 
-import mayflower.World;
+import mayflower.Keyboard;
+import mayflower.Mayflower;
 import org.rrhs.asteroids.GameState;
 import org.rrhs.asteroids.network.Client;
 
@@ -15,6 +16,26 @@ public class LobbyView extends GameView {
         super.showText("WEAPONS PRESS 1",50,100);
         super.showText("PILOT PRESS 2",50,150);
         super.showText("SENSORS PRESS 3",50,200);
-        super.showText("ENGINEER PRESS 4",50,100);
+        super.showText("ENGINEER PRESS 4",50,250);
+        if(Mayflower.isKeyPressed(Keyboard.KEY_1))
+        {
+            removeText(50,100);
+        }
+        if(Mayflower.isKeyPressed(Keyboard.KEY_2))
+        {
+            removeText(50, 150);
+        }
+        if(Mayflower.isKeyPressed(Keyboard.KEY_3))
+        {
+            removeText(50, 200);
+        }
+        if(Mayflower.isKeyPressed(Keyboard.KEY_4))
+        {
+            removeText(50, 250);
+        }
+    }
+    public void Temp(){
+        super.showText("PLEASE HOLD", 50, 50);
+        super.showText("OTHERS ARE CHOOSING", 50, 100);
     }
 }
