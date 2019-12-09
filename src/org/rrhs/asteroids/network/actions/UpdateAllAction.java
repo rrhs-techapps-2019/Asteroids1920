37935e8a2dde0bc336752c21c5a3854f352becad
jackson1442 <me@j1442.co>
2019-12-09 11:26:24 -0600

    reset

--- src/org/rrhs/asteroids/network/actions/UpdateAllAction.java
+++ src/org/rrhs/asteroids/network/actions/UpdateAllAction.java
@@ -3,18 +3,21 @@ package org.rrhs.asteroids.network.actions;
 import org.rrhs.asteroids.actors.NetworkActor;
 import org.rrhs.asteroids.network.Server;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UpdateAllAction implements NetworkAction {
     @Override
     public void act(Server server) {
+        Map<String, String> messageToSend = new HashMap<>();
 
         for (NetworkActor actor : actors.values())
         {
             messageToSend.put("action", "add");
             messageToSend.put("type", actor.getType());
             messageToSend.put("actor", actor.toString());
-            send(id, messageToSend.toString());
+            server.send(id, messageToSend.toString());
         }
-        break;
 
     }
 }
