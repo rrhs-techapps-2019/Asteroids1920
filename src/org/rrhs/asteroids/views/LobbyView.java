e2843ca1b71da53178291fe3d724c079cb1723d3
Villus007 <pvishwas2011@gmail.com>
2019-12-17 11:17:24 -0600

    New changes

--- src/org/rrhs/asteroids/views/LobbyView.java
+++ src/org/rrhs/asteroids/views/LobbyView.java
@@ -2,44 +2,53 @@ package org.rrhs.asteroids.views;
 
 import mayflower.Keyboard;
 import mayflower.Mayflower;
+import mayflower.World;
 import org.rrhs.asteroids.GameState;
+import org.rrhs.asteroids.RunnerOffline;
 import org.rrhs.asteroids.network.Client;
+import org.rrhs.asteroids.network.Server;
 
-public class LobbyView extends GameView {
+
+public class LobbyView extends World {
+    boolean wview = false;
+    boolean pview = false;
+    boolean sview = false;
+    boolean eview = false;
     public LobbyView(Client client, GameState state) {
-        super(client, state);
-    }
 
+    }
     @Override
     public void act() {
         super.showText("CHOOSE YOUR POSITION",50,50);
-        super.showText("WEAPONS PRESS 1",50,100);
+
+        if(!wview) super.showText("WEAPONS PRESS 1",50,100);
+
         super.showText("PILOT PRESS 2",50,150);
         super.showText("SENSORS PRESS 3",50,200);
         super.showText("ENGINEER PRESS 4",50,250);
         if(Mayflower.isKeyPressed(Keyboard.KEY_1))
         {
-            removeText(50,100);
-            Temp("WEAPONS");
+            super.removeText(50,100);
+            super.showText("Please Hold Weapons", 50, 100);
+            wview = true;
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_2))
         {
-            removeText(50, 150);
-            Temp("PILOT");
+            super.removeText(50, 150);
+            super.showText("Please Hold Pilot", 50, 150);
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_3))
         {
-            removeText(50, 200);
-            Temp("SENSORS");
+            super.removeText(50, 200);
+            super.showText("Please Hold Sensors", 50, 200);
         }
         if(Mayflower.isKeyPressed(Keyboard.KEY_4))
         {
-            removeText(50, 250);
-            Temp("ENGINEER");
+            super.removeText(50, 250);
+            super.showText("Please Hold Engineer", 50, 250);
         }
     }
-    public void Temp(String mc){
-        super.showText("PLEASE HOLD" + mc, 50, 50);
-        super.showText("OTHERS ARE CHOOSING", 50, 100);
+    public static void main(String[] args) {
+        new RunnerOffline(LobbyView.class);
     }
 }
