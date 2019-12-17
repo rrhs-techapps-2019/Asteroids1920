36bbc41040bceb2ac030b93fcaf1693a63080d1d
Hovey.Justin.s581974 <s581974@rrisd.local>
2019-12-17 13:27:55 -0600

    started rendering for all asteroids

--- src/org/rrhs/asteroids/views/FirstPersonView.java
+++ src/org/rrhs/asteroids/views/FirstPersonView.java
@@ -7,12 +7,11 @@ import org.rrhs.asteroids.network.Client;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public abstract class FirstPersonView extends GameView {
     private GameState gameState;
     private HashMap<NetworkActor, FPAsteroidActor> asteroidMap;
-    private ArrayList<FPAsteroidActor> asteroidCache;
-    private FPAsteroidActor fpaa;
     private int testRot;
     private double testCounter;
 
@@ -26,83 +25,31 @@ public abstract class FirstPersonView extends GameView {
         testCounter = 50;
         this.gameState = gameState;
         this.asteroidMap = new HashMap<>();
-        this.asteroidCache = new ArrayList<>(INIT_CACHE_SIZE);
-
-        for (int i = 0; i < INIT_CACHE_SIZE; i++) {
-            asteroidCache.add(new FPAsteroidActor());
-        }
 
         setBackground(new MayflowerImage(getWidth(), getHeight(), Color.BLACK));
-
-        fpaa = new FPAsteroidActor();
-        addObject(fpaa, -1000, 0);
     }
 
     public void act() {
-        //testCounter -= 0.1;
+        for (Map.Entry<NetworkActor, FPAsteroidActor> entry : asteroidMap.entrySet()) {
+            NetworkActor networkData = entry.getKey();
+            FPAsteroidActor actor = entry.getValue();
 
-        double xo = testCounter;
-        double yo = 0;
-        int asteroidX = getAsteroidX(xo, yo);
-        //System.out.println(testCounter);
-        double scale = Math.min(getScaleFactor(xo, yo) * 64, 1000);
-        //System.out.println("rot: " + testRot + " deg, asteroidx: " + asteroidX + ", scale: " + scale);
+            double xo = networkData.getX() - shipXGoesHere;
+            double yo = networkData.getY() - shipYGoesHere;
 
-        MayflowerImage img = new MayflowerImage("img/Asteroid.png");
-        img.scale((int) scale, (int) scale);
-        fpaa.setImage(img);
-        fpaa.setLocation(asteroidX - (fpaa.getImage().getWidth() / 2), getWidth() / 2 - (fpaa.getImage().getHeight() / 2));
-
-        if (Mayflower.isKeyDown(Keyboard.KEY_LEFT)) {
-            if (testRot == 360) {
-                testRot = 0;
-            }
-
-            testRot = testRot + 1;
-        } else if (Mayflower.isKeyDown(Keyboard.KEY_RIGHT)) {
-            if (testRot == 0) {
-                testRot = 360;
-            }
-
-            testRot = testRot - 1;
-        } else if (Mayflower.isKeyDown(Keyboard.KEY_DOWN)) {
-            testCounter -= 0.1;
-        } else if (Mayflower.isKeyDown(Keyboard.KEY_UP)) {
-            testCounter += 0.1;
+            int asteroidX = getAsteroidX(xo, yo);
+            double scale = getScaleFactor(xo, yo) * 64;
+            MayflowerImage img = new MayflowerImage("img/Asteroid.png");
+            img.scale((int) scale, (int) scale);
+            actor.setImage(img);
+            actor.setLocation(asteroidX - (actor.getImage().getWidth() / 2), getWidth() / 2 - (actor.getImage().getHeight() / 2));
         }
     }
 
     //returns the x-coordinate of an asteroid's sprite on the SSQ
     public int getAsteroidX(double trueX, double trueY) {
-        /*double θdeg = calculateRotation() % 360;
-        double θ = Math.toRadians(θdeg);
-        double relativeY = (trueY)*Math.cos(θ) + (trueX)*Math.sin(θ);
-
-        double φ = Math.atan2(trueY, trueX);
-        double ρ = Math.toRadians(FOV_ANGLE);
-        double μωμ = 0.5;*/
-        //double x = getWidth() * (((θ - φ) / ρ) /*- μωμ*/);
-        /*double x = getWidth() * (((θ - φ) / ρ) + μωμ);
-        System.out.println("rot: " + +θdeg + " deg, asteroidx: " + x + ", rel y: " + relativeY);
-
-        if (relativeY <= 0) {
-            //return Integer.MAX_VALUE;
-        } else {
-            //return (int) x;
-        }
-        return (int) x;*/
-
-        double θdeg = calculateRotation() % 360;
-
-
-
-
-
-
-
-
-
-        double θ = Math.toRadians(θdeg);
+        double γ = calculateRotation() % 360;
+        double θ = Math.toRadians(γ);
         double ρ = Math.toRadians(FOV_ANGLE);
         double φ = Math.atan2(trueY, trueX);
         double π = Math.PI;
@@ -115,16 +62,16 @@ public abstract class FirstPersonView extends GameView {
         }
 
         double μωμ = 0.5;
-        double x = getWidth() * ((ξ / ρ) + μωμ);
+        double χ = getWidth() * ((ξ / ρ) + μωμ);
 
-        System.out.println("rot: " + +θdeg + " deg, asteroidx: " + x);
+        System.out.println("rot: " + γ + " deg, asteroidx: " + χ);
 
-        return (int) x;
+        return (int) χ;
     }
 
     //returns the factor by which the asteroid sprite should be scaled for the perspective view
     public double getScaleFactor(double x, double y) {
-        return Math.max(0.01, SPRITE_SCALE/(Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0)) * FOV_ANGLE));
+        return Math.min(Math.max(0.01, SPRITE_SCALE/(Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0)) * FOV_ANGLE)), 1000s);
     }
 
     //pilot: returns ship rotation
@@ -148,9 +95,7 @@ public abstract class FirstPersonView extends GameView {
     class FPAsteroidActor extends Actor {
         public FPAsteroidActor() {
             MayflowerImage img = new MayflowerImage("img/Asteroid.png");
-            //img.scale(512, 512);
             setImage(img);
-            //scale(64, 64);
         }
 
         public void act() {
